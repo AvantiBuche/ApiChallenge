4ef1b1995cfadc4b62f640f5a27d977af97f59e6
Avanti Buche <127451991+AvantiBuche@users.noreply.github.com>
2025-12-16 23:28:38 +0530

    Update TOTP.py

--- TOTP.py
+++ TOTP.py
@@ -1,20 +1,45 @@
+import requests
+import json
+
 import hmac
 import hashlib
 import time
 import struct
 
-def generate_totp(email):
-    secret = (email + "HENNGECHALLENGE004").encode()
+def generate_totp(userid: str) -> str):
+    secret = (userid + "HENNGECHALLENGE004").encode("ascii")
     timestep = 30
-    counter = int(time.time() // timestep)
+    counter = int(time.time()) // timestep
 
     msg = struct.pack(">Q", counter)
     hmac_hash = hmac.new(secret, msg, hashlib.sha512).digest()
 
     offset = hmac_hash[-1] & 0x0F
-    binary = struct.unpack(">I", hmac_hash[offset:offset+4])[0] & 0x7fffffff
+    binary = struct.unpack(">I", hmac_hash[offset:offset+4])[0] 
+    binary &= 0x7FFFFFFF
 
     return str(binary % 10**10).zfill(10)
 
-email = "avibuche@example.com"
-print(generate_totp(email))
+url = "https://api.challenge.hennge.com/challenges/backend-recursion/004"
+email = "avibuche@gmail.com"
+totp_password = generate_totp(email)
+
+payload = {
+    "github_url": "https://gist.github.com/AvantiBuche/8c9abf85c386fe240efe4b498f02691f",
+    "contact_email": email,
+    "solution_language": "python"
+}
+
+headers = {
+    "Content-Type": "application/json"
+}
+
+response = requests.post(
+    url,
+    headers=headers,
+    data=json.dumps(payload),
+    auth=(email, totp_password)  # HTTP Basic Auth
+)
+
+print("Status Code:", response.status_code)
+print("Response Body:", response.text)
